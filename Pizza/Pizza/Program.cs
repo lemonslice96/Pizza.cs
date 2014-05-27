bcf57b44a7725f02714b43caf08763fb22cff133
lemonslice96 <lemonslice96@gmail.com>
2014-05-27 10:11:34 -0400

    fdbfbdf

---
+++ Pizza/Pizza/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _305_Probs
+{
+    class Pizza
+    {
+        static void Main(string[] args)
+        {
+            private string topping;
+
+            public string Topping
+            {
+                get
+                {
+                    return topping;
+                }
+                set
+                {
+                    topping = value;
+                }
+            }
+       
+    } 
+            class Program
+            {
+            public static void Main();
+        {
+            Pizza pizza = new Pizza();
+            Pizza.Topping = 'Pepperoni';
+        }
+        }
+}
+}
+
